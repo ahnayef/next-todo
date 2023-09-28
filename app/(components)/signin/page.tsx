03876b2596e40a3a0ab21e940ffae6f63f022ee4
AHNayef <ahnayef@outlook.com>
2023-09-29 01:09:50 +0600

    Login-Signup complete(UI)

--- app/(components)/signin/page.tsx
+++ app/(components)/signin/page.tsx
@@ -1,12 +1,20 @@
-import style from "./login.module.css"
+import style from "./signin.module.css"
 import { MdEmail } from 'react-icons/md';
 import { RiLockPasswordFill } from 'react-icons/ri';
+import { FaUser } from "react-icons/fa"
 
 export default function page() {
     return (
         <div className={style.loginMain}>
             <form>
-                <h1>Login</h1>
+                <h1>Sign Up</h1>
+
+
+                <div className={style.inputElem}>
+                    <i><FaUser /></i>
+                    <input id='uName' type="text" maxLength={20} placeholder='Name' required />
+                </div>
+
                 <div className={style.inputElem}>
                     <i><MdEmail /> </i>
                     <input id='uEmail' type="email" placeholder='Email' required />
@@ -16,7 +24,12 @@ export default function page() {
                     <i> <RiLockPasswordFill /> </i>
                     <input id='uPassword' type="password" placeholder='Password' required />
                 </div>
-                <button className={style.btn} type="submit">Login</button>
+
+                <div className={style.inputElem}>
+                    <i> <RiLockPasswordFill /> </i>
+                    <input id='uPassword' type="password" placeholder='Confirm Password' required />
+                </div>
+                <button className={style.btn} type="submit">Sign Up</button>
             </form>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
+import Link from 'next/link'
 import style from './page.module.css'
 
 export default function Home() {
   return (
     <div className={style.home}>
       <div className={style.left}>
-          <button>Log in</button>
-          <button>Sign up</button>
+          <Link href="/login" className={style.btn}>Log in</Link>
+          <Link href="/signin" className={style.btn}>Sign up</Link>
       </div>
       <div className={style.right}>
         <div className={style.todoBox}>
           <h1>Todo</h1>
           <p>Small steps, big impact.</p>
-          <button>Create your first todo</button>
+          <Link href="/todo" className={style.btn}>Create your first todo</Link>
         </div>
       </div>
     </div>
