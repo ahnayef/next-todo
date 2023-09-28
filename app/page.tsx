89c315f81a902f3799a98222eb0515cd72a662ea
AHNayef <ahnayef@outlook.com>
2023-09-28 10:33:14 +0600

    Running homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import styles from './page.module.css'
+import style from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-     <h1>Todo</h1>
-    </main>
+    <div className={style.home}>
+      <div className={style.left}>
+          <button>Log in</button>
+          <button>Sign up</button>
+      </div>
+      <div className={style.right}>
+        <div className={style.todoBox}>
+          <h1>Todo</h1>
+          <p>Small steps, big impact.</p>
+          <button>Create your first todo</button>
+        </div>
+      </div>
+    </div>
   )
 }
