2c2048a0c2b526958c23e5ebed293ac300aed774
AHNayef <ahnayef@outlook.com>
2023-09-29 20:50:48 +0600

    Add error page

--- app/(components)/Navbar/Navbar.tsx
+++ app/(components)/Navbar/Navbar.tsx
@@ -3,19 +3,28 @@
 import style from "./navbar.module.css"
 import "@/app/assets/icon/style.css"
 import Link from "next/link"
-import { FaUserCircle } from "react-icons/fa"
+import { FaEnvelope, FaInfoCircle, FaUserCircle } from "react-icons/fa"
+import { AiFillHome } from "react-icons/ai"
+import { IoIosCheckbox } from "react-icons/io"
+import { usePathname } from "next/navigation"
 
 export default function Navbar() {
 
+    const path = usePathname();
 
     return (
         <nav className={style.navMain}>
             <div className={style.logo}>
-               <Link href="/"> <i className="icon-check"></i> Todo</Link> 
+                <Link href="/"> <i className="icon-check"></i> Todo</Link>
             </div>
 
             <div className={style.navIcons}>
-                Navicons
+                <ul>
+                    <li><Link href="/" className={path === "/" ? "active" : ""}> <i><AiFillHome /></i> Home </Link></li>
+                    <li><Link href="/todo" className={path === "/todo" ? "active" : ""}> <i><IoIosCheckbox /></i> Todo </Link></li>
+                    <li><Link href="/about" className={path === "/about" ? "active" : ""}> <i><FaInfoCircle /></i> About </Link></li>
+                    <li><Link href="/contact" className={path === "/contact" ? "active" : ""}> <i><FaEnvelope /></i> Contact</Link></li>
+                </ul>
             </div>
             <div className={style.navIcons}>
                 <i><FaUserCircle /></i>
