eac17630935884527d54e889c60de0dc59f1e5ce
AHNayef <ahnayef@outlook.com>
2023-10-26 19:21:44 +0600

    Error handling

--- app/(components)/todos/Todos.tsx
+++ app/(components)/todos/Todos.tsx
@@ -6,8 +6,7 @@ import style from "./todos.module.css"
 import TodoBox from './TodoBox';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, db } from '@/app/firebase';
-import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
-import { toast } from 'react-toastify';
+import { collection, getDocs } from 'firebase/firestore';
 import Link from 'next/link';
 
 
@@ -17,7 +16,9 @@ export default function Todos() {
 
   const [user, loading, error] = useAuthState(auth);
 
-  const [isLoaded,setIsloaded]=useState(false);
+  const [isLoaded, setIsloaded] = useState(false);
+
+  const [isEmpty, setIsEmpty] = useState(true);
 
   useEffect(() => {
     if (loading) {
@@ -38,28 +39,30 @@ export default function Todos() {
 
   useEffect(() => {
 
-    if(user){
-      const getTodos = async ()=>{
-
-
-        const ref =collection(db, "users", `${user?.uid}`,"todos");
+    if (user) {
+      const getTodos = async () => {
+        const ref = collection(db, "users", `${user?.uid}`, "todos");
         const docSanp = await getDocs(ref);
 
-        let publicTodos:any = [];
-        let privateTodos:any = [];
+        if (!(docSanp.empty)) {
+          let publicTodos: any = [];
+          let privateTodos: any = [];
 
-        docSanp.forEach((doc)=>{
-          if(doc.data().private){
-            privateTodos.push(doc.data());
-          }else{
-            publicTodos.push(doc.data());
-          }
-        });
-        
-        setPublicTodos(publicTodos);
-        setPrivateTodos(privateTodos);
+          docSanp.forEach((doc) => {
+            if (doc.data().private) {
+              privateTodos.push(doc.data());
+            } else {
+              publicTodos.push(doc.data());
+            }
+          });
 
-        setIsloaded(true);
+          setPublicTodos(publicTodos);
+          setPrivateTodos(privateTodos);
+          setIsEmpty(false);
+          setIsloaded(true);
+        } else {
+          setIsloaded(true);
+        }
       }
 
       getTodos();
@@ -72,39 +75,39 @@ export default function Todos() {
     <>
       <div className={style.todosMain}>
         <h1>Your todos</h1>
-        <Link href="/createTodo" className='btn' style={{fontSize:"1.1em"}}>Create a new todo</Link>
-        {
-          !isLoaded ? <div className="loader"></div> : 
-          
+        <Link href="/createTodo" className='btn' style={{ fontSize: "1.01em" }}>Creare a new Todo</Link>
+
+        {!isEmpty ?
+
           <div className={style.toboxArea}>
-          <h2>Public:</h2>
-          <hr />
-          <div className={style.public}>
+            <h2>Public:</h2>
+            <hr />
+            <div className={style.public}>
 
-          {
-            publicTodos.map((todo:any)=>{
-              const progress= Math.round((todo.lists.filter((list:any)=>list.done).length/todo.lists.length)*100).toString();;
-              return <TodoBox tdTitle={todo.title} progress={progress} tid={todo.tid} key={todo.tid} />
-            })
-          }
+              {
+                publicTodos.map((todo: any) => {
+                  const progress = Math.round((todo.lists.filter((list: any) => list.done).length / todo.lists.length) * 100).toString();;
+                  return <TodoBox tdTitle={todo.title} progress={progress} tid={todo.tid} key={todo.tid} />
+                })
+              }
+
+            </div>
+            <h2>Private:</h2>
+            <hr />
+            <div className={style.private}>
+
+              {
+                privateTodos.map((todo: any) => {
+                  const progress = ((todo.lists.filter((list: any) => list.done).length / todo.lists.length) * 100).toString();
+                  return <TodoBox tdTitle={todo.title} progress={progress} tid={todo.tid} key={todo.tid} />
+                })
+              }
+
+            </div>
 
           </div>
-          <h2>Private:</h2>
-          <hr />
-          <div className={style.private}>
-
-          {
-            privateTodos.map((todo:any)=>{
-              const progress = ((todo.lists.filter((list:any)=>list.done).length/todo.lists.length)*100).toString();
-              return <TodoBox tdTitle={todo.title} progress={progress} tid={todo.tid} key={todo.tid} />
-            })
-          }
-
-          </div>
-
-        </div>
-        }
-
+          : <h2>You don't have any todos yet!</h2>}
+        {!isLoaded ? <div className="loader"></div> : null}
 
 
       </div>
