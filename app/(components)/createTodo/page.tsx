8c690ede6dc3cd99c5769694438b59686bcffd30
AHNayef <ahnayef@outlook.com>
2023-10-28 16:05:14 +0600

    Update title

--- app/(components)/createTodo/page.tsx
+++ app/(components)/createTodo/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from 'next'
 
 
 export const metadata: Metadata = {
-  title: 'Todogram',
+  title: 'Todogram | Create',
   description: 'Create a new todo',
   authors: [{ name: "AHNayef", url: "https://ahnayef.t.me" }],
   keywords: ["todo", "workflow", "management", "routine", "progress"],
