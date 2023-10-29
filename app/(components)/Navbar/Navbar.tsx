d7891352da5952a20a8ef3f219a21db575954a73
AHNayef <ahnayef@outlook.com>
2023-10-29 10:39:14 +0600

    Dynamic META

--- app/(components)/Navbar/Navbar.tsx
+++ app/(components)/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@
 import style from "./navbar.module.css"
 import "@/app/assets/icon/style.css"
 import Link from "next/link"
-import { FaEnvelope, FaInfoCircle, FaTimes } from "react-icons/fa"
+import { FaBars, FaEnvelope, FaInfoCircle, FaTimes } from "react-icons/fa"
 import { AiFillHome } from "react-icons/ai"
 import { IoIosCheckbox } from "react-icons/io"
 import { usePathname } from "next/navigation"
@@ -31,6 +31,13 @@ export default function Navbar() {
         }
     }, []);
 
+
+    const triggerNav = () => {
+        let check = document.getElementsByClassName("triggerNav")[0] as HTMLInputElement;
+        check.checked == true ? check.checked = false : check.checked = true;
+        setCheke(check.checked);
+    }
+
     const logOut = () => {
         track('logout from nav', {
             email: `${user?.email}`,
@@ -38,6 +45,7 @@ export default function Navbar() {
         });
         signOut(auth)
             .then(() => {
+                triggerNav();
                 toast.success("Logged out");
             })
             .catch((error) => {
@@ -50,44 +58,47 @@ export default function Navbar() {
     return (
         <>
             <ToastContainer theme="dark" />
-            <nav className={style.navMain}  ref={getHeight}>
+            <nav className={style.navMain} ref={getHeight}>
+                <input type="checkbox" className='triggerNav' id={style.navTg} hidden />
                 <div className={style.logo}>
                     <Link href="/"> <i className="icon-check"></i> Todo</Link>
                 </div>
 
                 <div className={style.navIcons}>
                     <ul>
-                        <li><Link href="/" className={path === "/" ? "active" : ""}> <i><AiFillHome /></i> Home </Link></li>
-                        <li><Link href="/todos" className={path === "/todos" ? "active" : ""}> <i><IoIosCheckbox /></i> Todos </Link></li>
-                        <li><Link href="/about" className={path === "/about" ? "active" : ""}> <i><FaInfoCircle /></i> About </Link></li>
-                        <li><Link href="/contact" className={path === "/contact" ? "active" : ""}> <i><FaEnvelope /></i> Contact</Link></li>
+                        <li onClick={triggerNav}><Link href="/" className={path === "/" ? "active" : ""}> <i><AiFillHome /></i> Home </Link></li>
+                        <li onClick={triggerNav}><Link href="/todos" className={path === "/todos" ? "active" : ""}> <i><IoIosCheckbox /></i> Todos </Link></li>
+                        <li onClick={triggerNav}><Link href="/about" className={path === "/about" ? "active" : ""}> <i><FaInfoCircle /></i> About </Link></li>
+                        <li onClick={triggerNav}><Link href="/contact" className={path === "/contact" ? "active" : ""}> <i><FaEnvelope /></i> Contact</Link></li>
                     </ul>
 
                     <div className={style.navProfile2}>
-                    <div className={style.navProfileMenu2}>
-                        <Link href={`/profile/${user?.uid}`} className="btn">Profile</Link>
-                        {
-                            user ?
-                                <div className="btn del" onClick={logOut}>Logout</div> : ""
-                        }
+                        <div className={style.navProfileMenu2}>
+                            <Link href={`/profile/${user?.uid}`} className="btn" onClick={triggerNav}>Profile</Link>
+                            {
+                                user ?
+                                    <div className="btn del" onClick={logOut}>Logout</div> : ""
+                            }
+                        </div>
+                        <button className="btn  del" onClick={triggerNav}><FaTimes /></button>
                     </div>
-                    <button className="btn  del"><FaTimes/></button>
-                </div>
 
                 </div>
 
                 <div className={style.navProfile}>
-                    <div className={style.navProfileMenu}>
-                        <Link href={`/profile/${user?.uid}`}>Profile</Link>
+                    <div className={style.navProfileMenu} >
+                        <Link href={`/profile/${user?.uid}`} onClick={triggerNav}>Profile</Link>
                         {
                             user ?
                                 <div className={`${style.btn} ${style.red}`} onClick={logOut}>Logout</div> : ""
                         }
                     </div>
                 </div>
-
+                <i className={style.navTg} onClick={triggerNav}>
+                    {check ? <FaTimes  style={{ color: "red" }} /> : <FaBars/>}
+                </i>
             </nav>
-            <div style={{height:navheight+20}} ></div>
+            <div style={{ height: navheight + 20 }} ></div>
         </>
     )
 }
--- app/(components)/todo/[tid]/page.tsx
+++ app/(components)/todo/[tid]/page.tsx
@@ -1,340 +1,83 @@
-"use client"
-
-import { auth, db } from "@/app/firebase";
-import { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { BiSolidEdit } from "react-icons/bi";
-import { FaCheck, FaCopy, FaLink, FaListUl, FaPlus, FaSave, FaShareAlt, FaTimes } from "react-icons/fa";
-import { FiTrash } from "react-icons/fi";
-import { ToastContainer, toast } from "react-toastify";
-import style from "./todo.module.css"
-import 'react-toastify/dist/ReactToastify.css';
-import axios from "axios";
-import { doc, updateDoc } from "firebase/firestore";
-import Link from "next/link";
-import { track } from "@vercel/analytics";
+import React from 'react'
+import Todo from './Todo'
+import { db } from '@/app/firebase';
+import { doc, getDoc } from 'firebase/firestore';
+import { Metadata } from 'next';
 
 
-const initialState: { tid: any, title: any, author: any, authorName: string, private: boolean, lists: Array<any> } = {
-  tid: "",
-  title: "",
-  author: "",
-  authorName: "",
-  private: false,
-  lists: []
-}
+type Props = {
+  params: { tid: string };
+};
 
-export default function Page({ params }: { params: { tid: string } }) {
-
+// set dynamic metadata
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // read route params
   const { tid } = params;
   const author = tid.split("!")[0];
   const utid = tid.split("!")[1];
-  
-  const [todoState, setTodoState] = useState(initialState);
-  const [todoProgress, setTodoProgress] = useState("0%");
-  const [isOwner, setISOwner] = useState(false);
-  const [copyData, setCopyData] = useState("");
 
+  let todoInfo: { title: string, authorName: string } = {
+    title: "",
+    authorName: "",
+  };
 
-  const [user, loading, error] = useAuthState(auth);
-  useEffect(() => {
-    if (loading) {
-      return;
-    }
-    else if (!user) {
-      location.href = '/login';
-    }
-    else if (error) {
-      console.log(error)
-    }
-  }, [user, loading, error]);
+  // fetch data from API
+  const todoRef = doc(db, "users", author, "todos", utid);
 
+  const todoSnap = await getDoc(todoRef);
 
-
-
-  // Load todo from db using tid
-
-  useEffect(() => {
-
-    if (`${user?.uid}` === author) {
-      setISOwner(true);
-    }
-
-    const getTodo = () => {
-      axios.post("/api/getSingleTodo", { user: `${user?.uid}`, author: author, tid: utid }).then((res) => {
-        const todo = res.data;
-        setTodoState(todo);
-        console.log(todo);
-        let data = JSON.stringify(todo);
-        setCopyData(encodeURIComponent(data));
-
-      }).catch(err => toast.error(err.response.data))
-    }
-
-    user && getTodo();
-
-  }, [tid, user]);
-
-
-
-
-
-  //Check progress
-  useEffect(() => {
-    if (todoState.lists.length > 0) {
-      let done = todoState.lists.filter((item: any) => item.done === true);
-      let progress = (done.length / todoState.lists.length) * 100;
-      setTodoProgress(progress + "%");
-    } else {
-      setTodoProgress("0%");
-    }
-  }, [todoState.lists]);
-
-
-  const [tempTask, setTempTask] = useState("");
-  const [toUpdate, setToUpdate] = useState({
-    id: "",
-    text: "",
-    done: false
-  });
-
-  const handleTitle = (e: any) => {
-    setTodoState({ ...todoState, title: e.target.value })
-  }
-
-  // Add Task
-  const addTast = () => {
-    if (tempTask) {
-      let num = todoState.lists.length + 1;
-      let newTask = { id: num, text: tempTask, done: false };
-      setTodoState({ ...todoState, lists: [...todoState.lists, newTask] });
-      setTempTask("");
-      track("Add a new task", { user: `${user?.uid}`,email:`${user?.email}` });
-
-    } else {
-      toast.error("Task can't be empty");
-    }
-  }
-
-  const deleteTask = (id: any) => {
-    let newTodo = todoState.lists.filter((item: any) => item.id !== id);
-    setTodoState({ ...todoState, lists: newTodo });
-    track("Delete a task", { user: `${user?.uid}`,email:`${user?.email}` });
-    if (toUpdate.id === id) {
-      setToUpdate({
-        id: "",
-        text: "",
-        done: false
-      });
-    }
+  if ((todoSnap.exists())) {
+    todoInfo = {
+      title: todoSnap.data().title,
+      authorName: todoSnap.data().authorName,
+    };
 
   }
 
-  const markDone = (id: any) => {
-    let newTodo = todoState.lists.map((item: any) => {
-      if (item.id === id) {
-        item.done = !item.done;
-      }
-      return item;
-    });
-    setTodoState({ ...todoState, lists: newTodo });
-    track("Mark a task done", { user: `${`${user?.uid}`}`,email:`${`${user?.email}`}` });
-    setToUpdate({
-      id: "",
-      text: "",
-      done: false
-    });
-  }
+  return {
+    title: todoInfo.title,
+    description: `A todo by ${todoInfo.authorName}`,
+    authors: [{ name: "AHNayef", url: "https://ahnayef.t.me" }],
+    keywords: ["todo", "workflow", "management", "routine", "progress"],
+    metadataBase: new URL('https://todogram.vercel.app'),
+    viewport: {
+      width: "device-width",
+      initialScale: 1,
+      userScalable: false
+    },
 
-  const cancelUpdate = () => {
-    setToUpdate({
-      id: "",
-      text: "",
-      done: false
-    });
-    track("Cancel updating a task", { user:`${`${user?.uid}`}`,email:`${`${user?.email}`}` });
-  }
-
-  const changeTask = (e: any) => {
-
-    let tempTodo = {
-      ...toUpdate,
-      text: e.target.value
+    openGraph: {
+      url: 'https://www.todogram.vercel.app',
+      siteName: 'Todogram',
+      images: [
+        {
+          url: "https://raw.githubusercontent.com/ahnayef/next-todo/main/app/assets/img/meta/todos.png",
+          width: 200,
+          height: 200,
+          alt: 'Meta Image',
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+    },
+    twitter: {
+      creator: "_AHNayef_",
+      images: [
+        {
+          url: "https://raw.githubusercontent.com/ahnayef/next-todo/main/app/assets/img/meta/todos.png",
+          width: 200,
+          height: 200,
+          alt: 'Meta Image',
+        },
+      ],
     }
-    setToUpdate(tempTodo);
-    track("Change task", { user: `${`${user?.uid}`}`,email:`${`${user?.email}`}` });
-  }
+  };
+}
 
-  const updateTask = () => {
-    let id = toUpdate.id;
-    let newTodo = todoState.lists.map((item: any) => {
-      if (item.id === id) {
-        item.text = toUpdate.text;
-      }
-      return item;
-    });
-
-    setTodoState({ ...todoState, lists: newTodo });
-    track("Update task", { user: `${`${user?.uid}`}`,email:`${`${user?.email}`}` });
-
-    setToUpdate({
-      id: "",
-      text: "",
-      done: false
-    });
-
-  }
-
-
-  const handleAddEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      addTast();
-      track("Add task using Enter key", { user: `${`${user?.uid}`}` ,email:`${`${user?.email}`}` })
-    }
-  }
-
-  const handleUpdateEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      updateTask();
-      track("Update a task using Enter key", { user: `${`${user?.uid}`}`,email:`${`${user?.email}`}` })
-    }
-  }
-
-  const saveTodo = () => {
-    if (todoState.title && todoState.lists.length > 0) {
-      let todo = {
-        private: todoState.private,
-        title: todoState.title,
-        lists: todoState.lists,
-      }
-
-      // Update todo here
-      const todoRef = doc(db, "users", `${`${user?.uid}`}`, "todos", `${tid.split("!")[1]}`);
-
-      updateDoc(todoRef, todo).then(() => {
-        toast.success("Todo saved successfully");
-        track("Save todo", { user: `${`${user?.uid}`}`,email:`${`${user?.email}`}` });
-        location.reload();
-      }
-      ).catch(err => console.log(err));
-
-
-    } else {
-      const target = document.querySelector("#tTitle") as HTMLInputElement;
-      target?.focus();
-      toast.error("Title can't be empty");
-    }
-  }
-
+export default function Page({ params }: { params: { tid: string } }) {
   return (
     <>
-      <ToastContainer theme='dark' />
-      <div className={style.sTodoMain}>
-
-        <h1>Todo</h1>
-
-        <ul>
-          {todoState ? <h1 className={style.title}> <i><FaListUl /></i> <input onChange={handleTitle} value={todoState?.title} id='tTitle' placeholder='Todo Title' disabled={isOwner ? false : true} /></h1> : ""}
-
-          {isOwner ?
-            <div className={style.actionArea}>
-
-              <div className={style.inputArea}>
-                <input type="text" placeholder="Add Task" value={tempTask} onChange={(e) => setTempTask(e.target.value)} onKeyDown={handleAddEnter} />
-                <button onClick={addTast}><i><FaPlus /></i></button>
-              </div>
-
-              <div className={style.inputArea}>
-                <input id="updateT" type="text" placeholder="Update Task" value={toUpdate && toUpdate.text} onChange={(e) => changeTask(e)} onKeyDown={handleUpdateEnter} disabled={toUpdate.id ? false : true} />
-                <div className={style.updateBtns}>
-                <button onClick={updateTask} disabled={toUpdate.id ? false : true} ><i><FaCheck /></i></button>
-                <button onClick={cancelUpdate} disabled={toUpdate.id ? false : true} ><FaTimes /></button>
-                </div>
-              </div>
-
-              <div className={style.privacyArea}>
-                <label htmlFor="tgBtn">Private: </label>
-                <label htmlFor='tgBtn' className={`${style.tBtn} ${style.b2} ${style.buttonTog}`}>
-                  <input type="checkbox" className={style.checkbox} id='tgBtn' onChange={() => {
-                    setTodoState({ ...todoState, private: !todoState.private })
-                  }} checked={todoState.private} hidden />
-                  <div className={style.knobs}>
-                    <span></span>
-                  </div>
-                </label>
-
-              </div>
-
-            </div> : null}
-
-          {todoState && todoState.lists.sort((a, b) => a.id > b.id ? 1 : -1).map((item: any, index: any) => {
-            return (
-              <li key={item.id} className={item.done ? style.done : ""}>
-
-                <div className={style.index}>
-                  {isOwner ?
-                    <p title='Mark done' onClick={() => markDone(item.id)}>
-                    </p>
-                    : <p>{index + 1}</p>}
-
-                </div>
-
-                <p className={style.text}>
-                  {item.text}
-                </p>
-
-                {isOwner ?
-                  <div className={style.tools}>
-                    <i title='Delete' onClick={() => deleteTask(item.id)}><FiTrash /></i>
-
-
-                    {!item.done ? <i title='Edit' onClick={() => {
-                      setToUpdate({
-                        id: item.id,
-                        text: item.text,
-                        done: false
-                      })
-
-                      const target = document.querySelector('#updateT') as HTMLInputElement;
-                      target.disabled = false;
-                      target?.focus();
-
-                    }}><BiSolidEdit /></i> : ''}
-
-                  </div>
-                  : null}
-              </li>
-            )
-          })}
-          <div className={style.progress}>
-            <div className={style.bar} style={{ width: `${todoProgress}` }}></div>
-          </div>
-          {
-            todoState.lists.length > 0 ? <>{isOwner ? <button onClick={saveTodo}> <FaSave />&nbsp;Save</button> : null} <div className={style.regularArea}>
-              <Link href={`/createTodo?datas=${copyData}`}><FaCopy />&nbsp;Duplicate</Link>
-              <button onClick={() => {
-                navigator.clipboard.writeText(`${location.origin}/todo/${tid}`);
-                navigator.vibrate(200);
-                toast.success("Link copied to clipboard");
-                track("Copy link", { user: `${`${user?.uid}`}`,email:`${`${user?.email}`}` });
-              }}><FaLink />&nbsp;Copy Link</button>
-              <button onClick={() => {
-                navigator.share({
-                  title: todoState.title,
-                  text: "Check out this todo",
-                  url: `${location.origin}/todo/${tid}`
-                }).then(()=>{
-                  track("Share todo", { user: `${`${user?.uid}`}`,email:`${`${user?.email}`}` });
-                });
-              }}><FaShareAlt />&nbsp;Share</button>
-            </div> </> : ""
-          }
-          <div className={style.authorArea}>
-            <p>Created by: <Link href={`/profile/${author}`}>{todoState.authorName}</Link></p>
-          </div>
-        </ul>
-      </div>
-
+      <Todo params={params} />
     </>
   )
 }
